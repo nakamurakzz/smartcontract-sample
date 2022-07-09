a3ba73f9e8ac257f1c2692712acc2983daed764c
nkmrkz <kazutaka.nakamura@ncdc.co.jp>
2022-07-09 15:55:08 +0900

    状態変数を変更する関数を追加

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,4 +1,11 @@
 pragma solidity ^0.8.15;
 contract GreeterContract {
+  string private _greeting = "Hello, World!";
+  function greet() external view returns (string memory) {
+    return _greeting;
+  }
 
+  function setGreeting(string calldata greeting) external {
+    _greeting = greeting;
+  }
 }
--- test/greeter_test.js
+++ test/greeter_test.js
@@ -9,4 +9,22 @@ contract("GreeterContract", ()=>{
     const greeter = await GreeterContract.deployed();
     assert(greeter, "contract was not deployed");
   })
+
+  describe("greet", ()=>{
+    it("should return Hello world", async ()=>{
+      const greeter = await GreeterContract.deployed();
+      const actual = await greeter.greet();
+      assert.equal(actual, "Hello, World!", "greeted message is not correct");
+    });
+  });
+});
+
+contract("GreeterContract", ()=>{
+  it("Set greeting(string)", async ()=>{
+    // ブロックチェーンのやり取りは全て非同期で行われる
+    const greeter = await GreeterContract.deployed();
+    await greeter.setGreeting("Hello, World!");
+    const actual = await greeter.greet();
+    assert.equal(actual, "Hello, World!", "greeted message is not correct");
+  })
 });
