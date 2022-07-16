12885579e6fba2d59a91149cbeb74efc80acdb51
nkmrkz <kazutaka.nakamura@ncdc.co.jp>
2022-07-16 17:43:54 +0900

    寄付機能の追加

--- fundraiser/truffle/contracts/Fundraiser.sol
+++ fundraiser/truffle/contracts/Fundraiser.sol
@@ -2,6 +2,16 @@ pragma solidity >0.4.23 <=0.8.15;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Fundraiser is Ownable {
+    struct Donation {
+        uint256 value;
+        uint256 date;
+    }
+
+    // キー：アドレスを寄付の配列に紐付けることが出来る
+    // マッピングは列挙型ではないため、forループはできない
+    // _donations[address]で寄付の履歴の配列を取得できる
+    mapping(address => Donation[]) private _donations;
+
     string public name;
     string public url;
     string public imageURL;
@@ -28,5 +38,34 @@ contract Fundraiser is Ownable {
 
     function setBeneficiary (address payable _beneficiary) public onlyOwner {
         beneficiary = _beneficiary;
+    }
+
+    function myDonationsCount() public view returns (uint256) {
+        return _donations[msg.sender].length;
+    }
+
+    function donate() public payable {
+        Donation memory donation = Donation({
+            value: msg.value,
+            date: block.timestamp
+        });
+        _donations[msg.sender].push(donation);
+    }
+
+    function myDonations() public view returns (
+        uint256[] memory values, uint256[] memory dates
+    ) {
+        // マッピングは列挙型ではないため、↓のようにはかけない
+        // values = _donations[msg.sender].map(donation => donation.value);
+
+        uint256 count = myDonationsCount();
+        values = new uint256[](count);
+        dates = new uint256[](count);
+
+        for (uint256 index = 0; index < count; index++) {
+            values[index] = _donations[msg.sender][index].value;
+            dates[index] = _donations[msg.sender][index].date;
+        }
+        return (values, dates);
     }
 }
--- fundraiser/truffle/test/founder_test.js
+++ fundraiser/truffle/test/founder_test.js
@@ -64,4 +64,23 @@ contract("Fundraiser", accounts => {
       }
     });
   });
+
+  describe("make donation", () => {
+    const value = web3.utils.toWei('0.0289');
+    const donor = accounts[2];
+
+    it("increases myDonationsCount", async () => {
+      const currentDonationsCount = await fundraiser.myDonationsCount({from:donor})
+      await fundraiser.donate({from:donor, value});
+      const newDonationsCount = await fundraiser.myDonationsCount({from:donor})      
+      assert.equal(1,newDonationsCount - currentDonationsCount,"myDonationsCount should increase")
+    });
+
+    it("includes donation in myDonations", async () => {
+      await fundraiser.donate({from:donor, value});
+      const {values, dates} = await fundraiser.myDonations({from:donor})
+      assert.equal(value,values[0],"value should match")
+      assert(dates[0],"date should be set")
+    });
+  });
 })
